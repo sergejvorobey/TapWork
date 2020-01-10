9dbe05386ce56ca0cddbbab25564af3210e9a341
sergeyvorobey <phanta2408@yandex.ru>
2020-01-10 21:13:13 +0300

    Initial Commit

---
+++ TapWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapWork
+//
+//  Created by Sergey Vorobey on 10/01/2020.
+//  Copyright © 2020 Сергей. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
